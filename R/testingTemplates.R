b36d114c07e1215c63778510c3063abce9bbe467
Baggerly,Keith A <kabagg@mdanderson.org>
2017-07-19 08:41:38 -0500

    rm NAMESPACE, document pkg, document, build

---
+++ R/testingTemplates.R
@@ -0,0 +1,9 @@
+#' This Package Helps Illustrate Template Tricks
+#'
+#' Here we have collected a set of tricks or pieces
+#' of code which we have found useful in formatting
+#' output reports in cleaner, more standardized ways.
+#'
+#' @docType package
+#' @name testingTemplates
+NULL
